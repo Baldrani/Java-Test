b90afee17b703cd10596c02b5fa3bc1dc3cd9f2c
Baldrani <mael.mayon@free.fr>
2018-05-29 23:18:46 +0200

    Better State

--- src/classes/Welcome.java
+++ src/classes/Welcome.java
@@ -1,20 +1,15 @@
 package classes;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class Logout extends HttpServlet{
+@WebServlet(urlPatterns="/index.xhtml")
+public class Welcome extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.invalidate();
-        response.sendRedirect("/test1/connexion");
+        this.getServletContext().getRequestDispatcher("/WEB-INF/welcome.jsp").forward(request, response);
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
-
 }
--- web/WEB-INF/welcome.jsp
+++ web/WEB-INF/welcome.jsp
@@ -3,11 +3,9 @@
 <t:layout>
 
     <jsp:attribute name="header">
-      <h1>Welcome</h1>
     </jsp:attribute>
 
     <jsp:attribute name="footer">
-      <p id="copyright">Copyright 1927, Future Bits When There Be Bits Inc.</p>
     </jsp:attribute>
 
     <jsp:body>
