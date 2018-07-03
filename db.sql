52eb0d1206af7ce65fb73318bd6b9913df1cd138
Baldrani <mael.mayon@free.fr>
2018-07-03 23:24:10 +0200

    Add db

---
+++ db.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS `url`;
+CREATE TABLE `url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `base` varchar(200) DEFAULT NULL,
+  `shortcut` varchar(200) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `starting_date` datetime,
+  `ending_date` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(100),
+  `email` varchar(100),
+  `mdp` varchar(100) DEFAULT NULL,
+  `type` varchar(100),
+  `create_at` datetime,
+  `modified_at` datetime,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `stat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `clicked_at` datetime DEFAULT NULL,
+  `id_url` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
